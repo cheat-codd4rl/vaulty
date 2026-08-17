cb5c891ec9345e5aae8e0ff4e279c1b32f176e28
Vaulty Dev <vaulty@example.com>
2026-08-17 17:52:51 +0300

    style: update brand logo to new PNG

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar() {
   return (
     <div className="topnav">
       <Link href="/" className="brand" aria-label="Vaulty home">
-        <img src="/vaulty-icon.svg" alt="Vaulty icon" className="brand-logo-icon" />
+        <img src="/vaulty-icon.png" alt="Vaulty icon" className="brand-logo-icon" />
         <span className="word">
           Vault<em>y</em>
         </span>
